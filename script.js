0d1f3cda46cd625c9730fe01da669911c3f348f2
Chad Harper <chadharper811@gmail.com>
2024-05-22 19:31:23 -0500

    minor changes

--- script.js
+++ script.js
@@ -9,3 +9,4 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
