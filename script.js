4cf082500f88e33a7f4ba7e4c56ccbbb32404d95
Chad Harper <chadharper811@gmail.com>
2024-05-27 12:16:08 -0500

    added modal functionality

--- script.js
+++ script.js
@@ -15,3 +15,8 @@ let currentTask = {};
 
 openTaskFormBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
 closeTaskFormBtn.addEventListener("click", () => confirmCloseDialog.showModal());
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+});
