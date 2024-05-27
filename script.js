e799944d1cd24d6191ad78069e3e958e2bc351ca
Chad Harper <chadharper811@gmail.com>
2024-05-27 12:34:05 -0500

    added event handler for creating new task

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
     taskForm.classList.toggle("hidden");
 });
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")-Date.now()}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value
+    };
+});
+
