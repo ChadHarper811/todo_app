b464bd038fa49f44fda1700da3c00a3d02f48dbc
Chad Harper <chadharper811@gmail.com>
2024-05-27 12:11:07 -0500

    added event listeners

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
+const taskData = [];
+let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
+closeTaskFormBtn.addEventListener("click", () => confirmCloseDialog.showModal());
